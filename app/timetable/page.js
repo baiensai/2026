f0c9055817c9a10170dc0af4a8a82837189ef600
cosmic-rare <ysolakey1116@gmail.com>
2025-08-05 13:38:51 +0900

    タイムテーブルの大枠完成

--- app/timetable/page.js
+++ app/timetable/page.js
@@ -1,10 +1,67 @@
+import { HiExclamation, HiCalendar, HiMap, HiQuestionMarkCircle } from "react-icons/hi"
+import { Accordion, AccordionContent, AccordionPanel, accordionTheme, AccordionTitle, ThemeProvider } from "flowbite-react"
+
 function TimeTable() {
   return (
     <div>
-      <p>日付の切り替え</p>
-      <p>場所</p>
-      <p>時</p>
-      <p>間</p>
+      <p className="text-3xl font-semibold text-center mt-6">1体タイムテーブル</p>
+
+      <div className="px-4 sm:px-10 mt-6 mb-8">
+
+
+        <div className="flex w-full">
+          <div className="w-1/7 bg-gray-100 p-2">
+            <div className='text-center'>時間</div>
+            <div className="h-[120px] text-center">9:30</div>
+            <div className="h-[120px] text-center">10:00</div>
+            <div className="h-[120px] text-center">10:30</div>
+            <div className="h-[120px] text-center">11:00</div>
+
+          </div>
+
+          <div className="w-3/7 bg-white py-2">
+            1日目
+            <div className="relative mx-4" style={{ top: '13px' }}>
+              <div className='absolute rounded-md bg-green-200 w-full shadow' style={{ top: '0px', height: '120px' }}>9:30-10:00</div>
+              <div className='absolute rounded-md bg-green-200 w-full shadow' style={{ top: '140px', height: '80px' }}>10:05-10:25</div>
+              <div className='absolute rounded-md bg-green-200 w-full shadow' style={{ top: '240px', height: '40px' }}>10:30-10:40</div>
+            </div>
+            <div className='relative' style={{ top: '13px' }}>
+              <div className='border-t-1 border-slate-700 h-[20px]'></div>
+              <div className='border-t-1 border-slate-700 h-[20px]'></div>
+              <div className='border-t-1 border-slate-700 h-[20px]'></div>
+              <div className='border-t-1 border-slate-700 h-[20px]'></div>
+              <div className='border-t-1 border-slate-700 h-[20px]'></div>
+              <div className='border-t-1 border-slate-700 h-[20px]'></div>
+              <div className='border-t-1 border-slate-700 h-[20px]'></div>
+              <div className='border-t-1 border-slate-700 h-[20px]'></div>
+              <div className='border-t-1 border-slate-700 h-[20px]'></div>
+              <div className='border-t-1 border-slate-700 h-[20px]'></div>
+              <div className='border-t-1 border-slate-700 h-[20px]'></div>
+            </div>
+          </div>
+
+          <div className="w-3/7 bg-white py-2 pr-2">
+          2日目
+          <div className='relative' style={{ top: '13px' }}>
+              <div className='border-t-1 border-slate-700 h-[20px]'></div>
+              <div className='border-t-1 border-slate-700 h-[20px]'></div>
+              <div className='border-t-1 border-slate-700 h-[20px]'></div>
+              <div className='border-t-1 border-slate-700 h-[20px]'></div>
+              <div className='border-t-1 border-slate-700 h-[20px]'></div>
+              <div className='border-t-1 border-slate-700 h-[20px]'></div>
+              <div className='border-t-1 border-slate-700 h-[20px]'></div>
+              <div className='border-t-1 border-slate-700 h-[20px]'></div>
+              <div className='border-t-1 border-slate-700 h-[20px]'></div>
+              <div className='border-t-1 border-slate-700 h-[20px]'></div>
+              <div className='border-t-1 border-slate-700 h-[20px]'></div>
+            </div>
+          </div>
+        </div>
+
+
+
+      </div>
     </div>
   )
 }
