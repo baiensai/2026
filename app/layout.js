00af9efe828f018504e454d76789659a6f0c0066
cosmic-rare <ysolakey1116@gmail.com>
2025-08-05 09:36:11 +0900

    どうでもいい

--- app/layout.js
+++ app/layout.js
@@ -49,7 +49,7 @@ export default function RootLayout({ children }) {
 
         <Footer container className="bg-pink-50 !rounded-none">
           <div className="w-full text-center">
-            <div className="w-full justify-between flex items-center justify-between">
+            <div className="w-full justify-between flex items-center">
               <FooterBrand
                 href="/"
                 src="logo.svg"
