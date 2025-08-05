947a0c3ef647f18650d4129c67dd45f60e3eb846
cosmic-rare <ysolakey1116@gmail.com>
2025-08-05 13:59:52 +0900

    タイムテーブルの枠をmapで生成するように

--- app/timetable/page.js
+++ app/timetable/page.js
@@ -1,7 +1,15 @@
 import { HiExclamation, HiCalendar, HiMap, HiQuestionMarkCircle } from "react-icons/hi"
 import { Accordion, AccordionContent, AccordionPanel, accordionTheme, AccordionTitle, ThemeProvider } from "flowbite-react"
 
+const BASE_HEIGHT = 20 //20px per 5min
+
 function TimeTable() {
+  const times = Array.from({ length: 12 }, (_, i) => {
+    const hour = 9 + Math.floor((i + 1) / 2)
+    const minute = (i + 1) % 2 === 0 ? '00' : '30'
+    return `${hour.toString().padStart(2, '0')}:${minute}`
+  })
+
   return (
     <div>
       <p className="text-3xl font-semibold text-center mt-6">1体タイムテーブル</p>
@@ -12,49 +20,35 @@ function TimeTable() {
         <div className="flex w-full">
           <div className="w-1/7 bg-gray-100 p-2">
             <div className='text-center'>時間</div>
-            <div className="h-[120px] text-center">9:30</div>
-            <div className="h-[120px] text-center">10:00</div>
-            <div className="h-[120px] text-center">10:30</div>
-            <div className="h-[120px] text-center">11:00</div>
+            {times.map((time) => (
+              <div key={time} className={`h-[${6 * BASE_HEIGHT}px] text-center`}>
+                {time}
+              </div>
+            ))}
 
           </div>
 
           <div className="w-3/7 bg-white py-2">
             1日目
             <div className="relative mx-4" style={{ top: '13px' }}>
-              <div className='absolute rounded-md bg-green-200 w-full shadow' style={{ top: '0px', height: '120px' }}>9:30-10:00</div>
-              <div className='absolute rounded-md bg-green-200 w-full shadow' style={{ top: '140px', height: '80px' }}>10:05-10:25</div>
-              <div className='absolute rounded-md bg-green-200 w-full shadow' style={{ top: '240px', height: '40px' }}>10:30-10:40</div>
+              <div className='z-10 absolute rounded-md bg-green-200 w-full shadow' style={{ top: '120px', height: '120px' }}>10:00-10:30</div>
             </div>
             <div className='relative' style={{ top: '13px' }}>
-              <div className='border-t-1 border-slate-700 h-[20px]'></div>
-              <div className='border-t-1 border-slate-700 h-[20px]'></div>
-              <div className='border-t-1 border-slate-700 h-[20px]'></div>
-              <div className='border-t-1 border-slate-700 h-[20px]'></div>
-              <div className='border-t-1 border-slate-700 h-[20px]'></div>
-              <div className='border-t-1 border-slate-700 h-[20px]'></div>
-              <div className='border-t-1 border-slate-700 h-[20px]'></div>
-              <div className='border-t-1 border-slate-700 h-[20px]'></div>
-              <div className='border-t-1 border-slate-700 h-[20px]'></div>
-              <div className='border-t-1 border-slate-700 h-[20px]'></div>
-              <div className='border-t-1 border-slate-700 h-[20px]'></div>
+              {Array.from({ length: 12 * 6 }, (_, i) => {
+                return <div className='border-t-1 border-slate-300 h-[20px]' key={i} />
+              })}
             </div>
           </div>
 
           <div className="w-3/7 bg-white py-2 pr-2">
-          2日目
-          <div className='relative' style={{ top: '13px' }}>
-              <div className='border-t-1 border-slate-700 h-[20px]'></div>
-              <div className='border-t-1 border-slate-700 h-[20px]'></div>
-              <div className='border-t-1 border-slate-700 h-[20px]'></div>
-              <div className='border-t-1 border-slate-700 h-[20px]'></div>
-              <div className='border-t-1 border-slate-700 h-[20px]'></div>
-              <div className='border-t-1 border-slate-700 h-[20px]'></div>
-              <div className='border-t-1 border-slate-700 h-[20px]'></div>
-              <div className='border-t-1 border-slate-700 h-[20px]'></div>
-              <div className='border-t-1 border-slate-700 h-[20px]'></div>
-              <div className='border-t-1 border-slate-700 h-[20px]'></div>
-              <div className='border-t-1 border-slate-700 h-[20px]'></div>
+            2日目
+            <div className="relative mx-4" style={{ top: '13px' }}>
+              <div className='z-10 absolute rounded-md bg-purple-200 w-full shadow' style={{ top: '380px', height: '80px' }}>11:05-11:25</div>
+            </div>
+            <div className='relative' style={{ top: '13px' }}>
+              {Array.from({ length: 12 * 6 }, (_, i) => {
+                return <div className='border-t-1 border-slate-300 h-[20px]' key={i} />
+              })}
             </div>
           </div>
         </div>
