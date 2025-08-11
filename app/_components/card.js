19d5ba0ef49233dd72dc6ebb88dc6633041a4553
cosmic-rare <ysolakey1116@gmail.com>
2025-08-11 23:45:34 +0900

    移動

--- app/_components/card.js
+++ app/_components/card.js
@@ -1,7 +1,25 @@
 function Card() {
   return (
-    <div>
-      イラスト写真、ジャンル種類、タイトル
+    <div className="w-full p-1 border-[4px] border-pink-500 rounded-xl">
+
+      <div className="flex">
+        <img src='/test-icon.webp' className="w-25 h-25" />
+        <div>
+          <p>！場所</p>
+          <p>！ジャンル</p>
+          <p>時間(あれば)</p>
+        </div>
+      </div>
+
+      <div className="flex">
+        <div>
+          <p>タイトル</p>
+          <p>団体名</p>
+        </div>
+        <p>ハート</p>
+      </div>
+
+      <p>説明文の最初の部分をここにいれたいんだけど長いだろうから途中で切って....って感じにしたいよね</p>
     </div>
   )
 }
--- app/projects/page.js
+++ app/projects/page.js
@@ -1,12 +1,17 @@
+import Card from "../_components/card"
+
 function Projects() {
   return (
-    <div>
-      <p>企画一覧</p>
+    <div className="flex space-x-3 px-3">
+      <Card />
+      <Card />
+      <Card />
+      {/* <p>企画一覧</p>
       <p>企画の種類について</p>
       <p>賞について</p>
       <p>種類、ジャンル、検索の欄</p>
       <p>n件ヒット</p>
-      <p>いっぱいのカード</p>
+      <p>いっぱいのカード</p> */}
     </div>
   )
 }
