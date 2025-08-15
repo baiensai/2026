ffb6598fa3d9f47b4979349b9fce0003057b54c3
cosmic-rare <ysolakey1116@gmail.com>
2025-08-15 12:00:16 +0900

    企画一覧をデータからmapで表示するようにしてfuse.jsを導入した

--- app/_components/card.js
+++ app/_components/card.js
@@ -1,20 +1,20 @@
 import { IoMdPin, IoMdHeart } from "react-icons/io"
 
-function Card() {
+function Card({ data }) {
   return (
     <div className="w-full p-1 border-[2px] border-red-300 rounded-xl bg-[#FFFFFF]">
-
       <div className="flex flex-col md:flex-row items-center">
-        <img src='/test-icon.webp' className="w-15 h-15 md:w-20 md:h-20 rounded-md mr-auto md:mr-2" />
+        <img src={data.icon} className="w-15 h-15 md:w-20 md:h-20 rounded-md mr-auto md:mr-2" />
         <div className="w-full mt-2 md:mt-0">
           <div className="flex items-center mb-1">
             <IoMdPin className="w-4 h-4 md:w-5 md:h-5 mr-1" />
-            <p className="text-sm md:text-base">3棟2階 3-2教室</p>
+            <p className="text-sm md:text-base">{data.place}</p>
           </div>
           <div className="flex items-center justify-between w-full">
             <div className="flex gap-1">
-              <p className="border-2 rounded-full border-blue-500 bg-blue-100 px-1 text-xs md:text-sm">クラス企画</p>
-              <p className="border-2 rounded-full border-blue-500 bg-blue-100 px-1 text-xs md:text-sm">劇</p>
+              {data.tags.map((tag, i) => (
+                <p className={`border-2 rounded-full border-${tag.color}-500 bg-${tag.color}-100 px-1 text-xs md:text-sm`} key={i}>{tag.text}</p>
+              ))}
             </div>
             {/* <IoMdHeart className="w-6 h-6 mr-1 text-red-600" /> これ実装する余裕あるかわからないのでコメントで残しときます */}
           </div>
@@ -23,12 +23,12 @@ function Card() {
 
       <div className="flex my-2">
         <div>
-          <p className="text-base md:text-lg text-rose-600 font-semibold">あへあへあへあへ</p>
-          <p className="text-xs md:text-sm text-gray-500">部活のときとかの</p>
+          <p className="text-base md:text-lg text-rose-600 font-semibold">{data.title}</p>
+          <p className="text-xs md:text-sm text-gray-500">{data.organization}</p>
         </div>
       </div>
 
-      <p className="text-sm md:text-base w-full line-clamp-2 md:line-clamp-3">レイアウトをパクるのにとどまらずテストの文章まで五月祭のサイトから引っこ抜いてくるなど地球惑星科学を専攻する学生が主催する企画です！展示や実験、シミュレーションのほか雑誌や地層パフェの提供も行います！地球惑星科学を専攻する学生が主催する企画です！展示や実験、シミュレーションのほか雑誌や地層パフェの提供も行います！地球惑星科学を専攻する学生が主催する企画です！展示や実験、シミュレーションのほか雑誌や地層パフェの提供も行います！</p>
+      <p className="text-sm md:text-base w-full line-clamp-2 md:line-clamp-3">{data.description}</p>
     </div>
   )
 }
--- app/projects/page.js
+++ app/projects/page.js
@@ -1,30 +1,22 @@
 import Card from "../_components/card"
-{/* <div className="flex flex-col md:flex-row md:space-x-4 md:px-3 mx-8 md:mx-0 space-y-4 md:space-y-0"> */}
+import datas from "./_data"
+import { useState, useEffect } from "react"
+import Fuse from "fuse.js"
 
 function Projects() {
   return (
-<div className="grid grid-cols-2 md:grid-cols-3 gap-y-2 md:gap-y-5 gap-x-2 md:gap-x-5 px-2 md:px-12 max-w-[1000px] mx-auto">      
-    <Card />
-      <Card />
-      <Card />      <Card />
-      <Card />
-      <Card />      <Card />
-      <Card />
-      <Card />      <Card />
-      <Card />
-      <Card />      <Card />
-      <Card />
-      <Card />      <Card />
-      <Card />
-      <Card />      <Card />
-      <Card />
-      <Card />
+    <div className="grid grid-cols-2 md:grid-cols-3 gap-y-2 md:gap-y-5 gap-x-2 md:gap-x-5 px-2 md:px-12 max-w-[1000px] mx-auto">
+      {datas.map((data, i) => (
+        <Card data={data} key={i} />
+      ))}
+
       {/* <p>企画一覧</p>
       <p>企画の種類について</p>
       <p>賞について</p>
       <p>種類、ジャンル、検索の欄</p>
       <p>n件ヒット</p>
       <p>いっぱいのカード</p> */}
+      <div className="hidden"><p className="border-blue-500 bg-blue-100" /></div>
     </div>
   )
 }
