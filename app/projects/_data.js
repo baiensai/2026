3c097f1fc2039bacdeba0cbb1a3a174adc887893
cosmic-rare <ysolakey1116@gmail.com>
2025-08-15 14:57:31 +0900

    今集まってる有志つっこんだ

--- app/projects/_data.js
+++ app/projects/_data.js
@@ -1,5 +1,48 @@
 const datas = [
   {
+    icon: "/icons/3.png",
+    place: "1体後方",
+    tags: [
+      { color: "blue", text: "パフォーマンス" },
+      { color: "blue", text: "有志" },
+    ],
+    title: "書道部",
+    organization: "書道部",
+    description: "書道部員が3m×5mもの大きな紙の上で書のパフォーマンスを披露します!"
+  },
+  {
+    icon: "/icons/12.png",
+    place: "1体ステージ",
+    tags: [
+      { color: "blue", text: "ダンス" },
+      { color: "blue", text: "DJ" },
+    ],
+    title: "All for D",
+    organization: "",
+    description: ""
+  },
+  {
+    icon: "/icons/17.png",
+    place: "1体ステージ",
+    tags: [
+      { color: "blue", text: "バンド" },
+    ],
+    title: "takogeso",
+    organization: "",
+    description: "",
+    modalDescription: <img src="/test-icon.webp" />
+  },
+  {
+    icon: "/icons/22.png",
+    place: "1体ステージ",
+    tags: [
+      { color: "blue", text: "ビブリオバトル" },
+    ],
+    title: "図書委員会",
+    organization: "図書委員会",
+    description: "「バトラーおすすめの本をご本人に熱く語って頂き、一番読みたくなった本No. 1を決めることが主な内容となっております。観客の方には最後、チャンプ本にしたい本を選んで頂きます！本好きも、そうでない人も、疲れずゆったりと楽しめるものとなっておりますので、ぜひご参加ください！」"
+  },
+  {
     icon: "/test-icon.webp",
     place: "3棟2階 3-2教室",
     tags: [
@@ -9,7 +52,8 @@ const datas = [
     title: "あへあへあへあへ",
     organization: "部活のときとかの",
     description: "レイアウトをパクるのにとどまらずテストの文章まで五月祭のサイトから引っこ抜いてくるなど地球惑星科学を専攻する学生が主催する企画です！展示や実験、シミュレーションのほか雑誌や地層パフェの提供も行います！地球惑星科学を専攻する学生が主催する企画です！展示や実験、シミュレーションのほか雑誌や地層パフェの提供も行います！地球惑星科学を専攻する学生が主催する企画です！展示や実験、シミュレーションのほか雑誌や地層パフェの提供も行います！"
-  }
+  },
+  
 ]
 
 export default datas
