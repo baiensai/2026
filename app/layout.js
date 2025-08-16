4bc0bdc6d62ecb4cd776f7f8c09d74024a449381
cosmic-rare <ysolakey1116@gmail.com>
2025-08-17 03:46:02 +0900

    favicon and title

--- app/layout.js
+++ app/layout.js
@@ -16,13 +16,19 @@ const geistMono = Geist_Mono({
 })
 
 export const metadata = {
-  title: "baiensai webpage",
-  description: "",
+  title: "福島高校文化祭 梅苑祭2025 公式サイト",
+  description: "福島高校文化祭 梅苑祭2025「EXCEED POTENTIAL」の公式サイト",
 }
 
 export default function RootLayout({ children }) {
   return (
     <html lang="en">
+      <head>
+        <link rel="apple-touch-icon" sizes="180x180" href="/apple-touch-icon.png" />
+        <link rel="icon" type="image/png" sizes="32x32" href="/favicon-32x32.png" />
+        <link rel="icon" type="image/png" sizes="16x16" href="/favicon-16x16.png" />
+        <link rel="manifest" href="/site.webmanifest" />
+      </head>
       <body
         className={`${geistSans.variable} ${geistMono.variable} antialiased !bg-[#FCFCFC]`}
       >
