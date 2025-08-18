ba4bf827236c31cd838692b7c5f50fa427d04132
cosmic-rare <ysolakey1116@gmail.com>
2025-08-18 13:48:59 +0900

    微妙な幅で中央にならないやつ修正

--- app/map/page.js
+++ app/map/page.js
@@ -83,105 +83,107 @@ export default function Map() {
   }
 
   return (
-    <div className="px-4 sm:px-10 mx-2 md:mx-auto max-w-[700px]">
-      <p className="text-3xl font-semibold text-center mt-6 mb-6">校内マップ</p>
+    <div className="flex justify-center">
+      <div className="px-4 sm:px-10 mx-2 md:mx-auto max-w-[700px]">
+        <p className="text-3xl font-semibold text-center mt-6 mb-6">校内マップ</p>
 
 
-      <div className="flex justify-between items-center mb-4">
-        <p className="text-xl font-semibold">3,4棟</p>
-        <FloorSelector34 setSelected34={setSelected34} selected34={selected34} />
-      </div>
+        <div className="flex justify-between items-center mb-4">
+          <p className="text-xl font-semibold">3,4棟</p>
+          <FloorSelector34 setSelected34={setSelected34} selected34={selected34} />
+        </div>
 
-      <div className="w-full aspect-[1]">
-        <TransformWrapper wheel={{ step: 0.1 }} pinch={{ step: 5 }} doubleClick={{ disabled: true }}>
-          {({ state }) => (
-            <TransformComponent
-              wrapperStyle={{ width: "100%", height: "100%", overflow: "hidden" }}
-              contentStyle={{ width: "fit-content", height: "fit-content" }}
-            >
-              <div className="relative inline-block" ref={containerRef}>
-                <img src={`/maps/${selected34}.png`} alt="illustration" className="block" />
-                <div
-                  className="absolute top-0 left-0 w-full h-full cursor-pointer"
-                  onClick={handleClick}
-                />
-              </div>
-            </TransformComponent>
-          )}
-        </TransformWrapper>
-      </div>
-
-
-      <div className="flex justify-between items-center mb-4 mt-12">
-        <p className="text-xl font-semibold">1,2棟</p>
-        <FloorSelector12 setSelected12={setSelected12} selected12={selected12} />
-      </div>
-
-      <div className="w-full aspect-[1.16]">
-        <TransformWrapper wheel={{ step: 0.1 }} pinch={{ step: 5 }} doubleClick={{ disabled: true }}>
-          {({ state }) => (
-            <TransformComponent
-              wrapperStyle={{ width: "100%", height: "100%", overflow: "hidden" }}
-              contentStyle={{ width: "fit-content", height: "fit-content" }}
-            >
-              <div className="relative inline-block" ref={containerRef}>
-                <img src={`/maps/${selected12 + 4}.png`} alt="illustration" className="block" />
-                <div
-                  className="absolute top-0 left-0 w-full h-full cursor-pointer"
-                  onClick={handleClick}
-                />
-              </div>
-            </TransformComponent>
-          )}
-        </TransformWrapper>
-      </div>
+        <div className="w-full aspect-[1]">
+          <TransformWrapper wheel={{ step: 0.1 }} pinch={{ step: 5 }} doubleClick={{ disabled: true }}>
+            {({ state }) => (
+              <TransformComponent
+                wrapperStyle={{ width: "100%", height: "100%", overflow: "hidden" }}
+                contentStyle={{ width: "fit-content", height: "fit-content" }}
+              >
+                <div className="relative inline-block" ref={containerRef}>
+                  <img src={`/maps/${selected34}.png`} alt="illustration" className="block" />
+                  <div
+                    className="absolute top-0 left-0 w-full h-full cursor-pointer"
+                    onClick={handleClick}
+                  />
+                </div>
+              </TransformComponent>
+            )}
+          </TransformWrapper>
+        </div>
 
 
-      <div className="flex justify-between items-center mb-4 mt-12">
-        <p className="text-xl font-semibold">1体</p>
-      </div>
+        <div className="flex justify-between items-center mb-4 mt-12">
+          <p className="text-xl font-semibold">1,2棟</p>
+          <FloorSelector12 setSelected12={setSelected12} selected12={selected12} />
+        </div>
 
-      <div className="w-full aspect-[1.38]">
-        <TransformWrapper wheel={{ step: 0.1 }} pinch={{ step: 5 }} doubleClick={{ disabled: true }}>
-          {({ state }) => (
-            <TransformComponent
-              wrapperStyle={{ width: "100%", height: "100%", overflow: "hidden" }}
-              contentStyle={{ width: "fit-content", height: "fit-content" }}
-            >
-              <div className="relative inline-block" ref={containerRef}>
-                <img src={`/maps/8.png`} alt="illustration" className="block" />
-                <div
-                  className="absolute top-0 left-0 w-full h-full cursor-pointer"
-                  onClick={handleClick}
-                />
-              </div>
-            </TransformComponent>
-          )}
-        </TransformWrapper>
-      </div>
+        <div className="w-full aspect-[1.16]">
+          <TransformWrapper wheel={{ step: 0.1 }} pinch={{ step: 5 }} doubleClick={{ disabled: true }}>
+            {({ state }) => (
+              <TransformComponent
+                wrapperStyle={{ width: "100%", height: "100%", overflow: "hidden" }}
+                contentStyle={{ width: "fit-content", height: "fit-content" }}
+              >
+                <div className="relative inline-block" ref={containerRef}>
+                  <img src={`/maps/${selected12 + 4}.png`} alt="illustration" className="block" />
+                  <div
+                    className="absolute top-0 left-0 w-full h-full cursor-pointer"
+                    onClick={handleClick}
+                  />
+                </div>
+              </TransformComponent>
+            )}
+          </TransformWrapper>
+        </div>
 
 
-      <div className="flex justify-between items-center mb-4 mt-12">
-        <p className="text-xl font-semibold">2体</p>
-      </div>
+        <div className="flex justify-between items-center mb-4 mt-12">
+          <p className="text-xl font-semibold">1体</p>
+        </div>
 
-      <div className="w-full aspect-[1.2]">
-        <TransformWrapper wheel={{ step: 0.1 }} pinch={{ step: 5 }} doubleClick={{ disabled: true }}>
-          {({ state }) => (
-            <TransformComponent
-              wrapperStyle={{ width: "100%", height: "100%", overflow: "hidden" }}
-              contentStyle={{ width: "fit-content", height: "fit-content" }}
-            >
-              <div className="relative inline-block" ref={containerRef}>
-                <img src={`/maps/9.png`} alt="illustration" className="block" />
-                <div
-                  className="absolute top-0 left-0 w-full h-full cursor-pointer"
-                  onClick={handleClick}
-                />
-              </div>
-            </TransformComponent>
-          )}
-        </TransformWrapper>
+        <div className="w-full aspect-[1.38]">
+          <TransformWrapper wheel={{ step: 0.1 }} pinch={{ step: 5 }} doubleClick={{ disabled: true }}>
+            {({ state }) => (
+              <TransformComponent
+                wrapperStyle={{ width: "100%", height: "100%", overflow: "hidden" }}
+                contentStyle={{ width: "fit-content", height: "fit-content" }}
+              >
+                <div className="relative inline-block" ref={containerRef}>
+                  <img src={`/maps/8.png`} alt="illustration" className="block" />
+                  <div
+                    className="absolute top-0 left-0 w-full h-full cursor-pointer"
+                    onClick={handleClick}
+                  />
+                </div>
+              </TransformComponent>
+            )}
+          </TransformWrapper>
+        </div>
+
+
+        <div className="flex justify-between items-center mb-4 mt-12">
+          <p className="text-xl font-semibold">2体</p>
+        </div>
+
+        <div className="w-full aspect-[1.2]">
+          <TransformWrapper wheel={{ step: 0.1 }} pinch={{ step: 5 }} doubleClick={{ disabled: true }}>
+            {({ state }) => (
+              <TransformComponent
+                wrapperStyle={{ width: "100%", height: "100%", overflow: "hidden" }}
+                contentStyle={{ width: "fit-content", height: "fit-content" }}
+              >
+                <div className="relative inline-block" ref={containerRef}>
+                  <img src={`/maps/9.png`} alt="illustration" className="block" />
+                  <div
+                    className="absolute top-0 left-0 w-full h-full cursor-pointer"
+                    onClick={handleClick}
+                  />
+                </div>
+              </TransformComponent>
+            )}
+          </TransformWrapper>
+        </div>
       </div>
     </div>
   )
