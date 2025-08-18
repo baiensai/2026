5728318917f91507ee9a2fdadb1aca127a09b11b
cosmic-rare <ysolakey1116@gmail.com>
2025-08-18 15:32:13 +0900

    DRY

--- app/map/page.js
+++ app/map/page.js
@@ -17,19 +17,19 @@ const floors12 = [
   { value: 3, label: "3階" },
 ]
 
-function FloorSelector34({ onSelect, setSelected34, selected34 }) {
+function FloorSelector({ onSelect, setSelected, selected, floors }) {
   const handleSelect = (floor) => {
-    setSelected34(floor)
+    setSelected(floor)
     if (onSelect) onSelect(floor)
   }
 
   return (
     <div className="">
       <div className="flex gap-3">
-        {floors34.map((f) => (
+        {floors.map((f) => (
           <Button
             key={f.value}
-            color={selected34 === f.value ? "pink" : "light"}
+            color={selected === f.value ? "pink" : "light"}
             pill
             size="sm"
             onClick={() => handleSelect(f.value)}
@@ -42,27 +42,24 @@ function FloorSelector34({ onSelect, setSelected34, selected34 }) {
   )
 }
 
-function FloorSelector12({ onSelect, setSelected12, selected12 }) {
-  const handleSelect = (floor) => {
-    setSelected12(floor)
-    if (onSelect) onSelect(floor)
-  }
-
+function ZoomableMap({ aspect, imgSrc }) {
   return (
-    <div className="">
-      <div className="flex gap-3">
-        {floors12.map((f) => (
-          <Button
-            key={f.value}
-            color={selected12 === f.value ? "pink" : "light"}
-            pill
-            size="sm"
-            onClick={() => handleSelect(f.value)}
+    <div className={`w-full aspect-[${aspect}]`}>
+      <TransformWrapper wheel={{ step: 0.1 }} pinch={{ step: 5 }} doubleClick={{ disabled: true }}>
+        {({ state }) => (
+          <TransformComponent
+            wrapperStyle={{ width: "100%", height: "100%", overflow: "hidden" }}
+            contentStyle={{ width: "fit-content", height: "fit-content" }}
           >
-            {f.label}
-          </Button>
-        ))}
-      </div>
+            <div className="relative inline-block">
+              <img src={imgSrc} alt="illustration" className="block" />
+              <div className="absolute top-0 left-0 w-full h-full cursor-pointer">
+                <div className="h-[20px] w-[20px] bg-sky-400 absolute top-[20px] bottom[20px] rounded-full" onClick={() => console.log('aheahe')} />
+              </div>
+            </div>
+          </TransformComponent>
+        )}
+      </TransformWrapper>
     </div>
   )
 }
@@ -76,99 +73,28 @@ export default function Map() {
       <div className="px-4 sm:px-10 mx-2 md:mx-auto max-w-[700px]">
         <p className="text-3xl font-semibold text-center mt-6 mb-6">校内マップ</p>
 
-
         <div className="flex justify-between items-center mb-4">
           <p className="text-xl font-semibold">3,4棟</p>
-          <FloorSelector34 setSelected34={setSelected34} selected34={selected34} />
+          <FloorSelector setSelected={setSelected34} selected={selected34} floors={floors34} />
         </div>
-
-        <div className="w-full aspect-[1]">
-          <TransformWrapper wheel={{ step: 0.1 }} pinch={{ step: 5 }} doubleClick={{ disabled: true }}>
-            {({ state }) => (
-              <TransformComponent
-                wrapperStyle={{ width: "100%", height: "100%", overflow: "hidden" }}
-                contentStyle={{ width: "fit-content", height: "fit-content" }}
-              >
-                <div className="relative inline-block">
-                  <img src={`/maps/${selected34}.png`} alt="illustration" className="block" />
-                  <div className="absolute top-0 left-0 w-full h-full cursor-pointer">
-                    <div className="h-[20px] w-[20px] bg-sky-400 absolute top-[20px] bottom[20px] rounded-full" onClick={() => console.log('aheahe')} />
-                  </div>
-                </div>
-              </TransformComponent>
-            )}
-          </TransformWrapper>
-        </div>
-
+        <ZoomableMap aspect={'1'} imgSrc={`/maps/${selected34}.png`} />
 
         <div className="flex justify-between items-center mb-4 mt-12">
           <p className="text-xl font-semibold">1,2棟</p>
-          <FloorSelector12 setSelected12={setSelected12} selected12={selected12} />
+          <FloorSelector setSelected={setSelected12} selected={selected12} floors={floors12} />
         </div>
-
-        <div className="w-full aspect-[1.16]">
-          <TransformWrapper wheel={{ step: 0.1 }} pinch={{ step: 5 }} doubleClick={{ disabled: true }}>
-            {({ state }) => (
-              <TransformComponent
-                wrapperStyle={{ width: "100%", height: "100%", overflow: "hidden" }}
-                contentStyle={{ width: "fit-content", height: "fit-content" }}
-              >
-                <div className="relative inline-block">
-                  <img src={`/maps/${selected12 + 4}.png`} alt="illustration" className="block" />
-                  <div
-                    className="absolute top-0 left-0 w-full h-full cursor-pointer"
-                  />
-                </div>
-              </TransformComponent>
-            )}
-          </TransformWrapper>
-        </div>
-
+        <ZoomableMap aspect={'1.16'} imgSrc={`/maps/${selected12 + 4}.png`} />
 
         <div className="flex justify-between items-center mb-4 mt-12">
           <p className="text-xl font-semibold">1体</p>
         </div>
-
-        <div className="w-full aspect-[1.38]">
-          <TransformWrapper wheel={{ step: 0.1 }} pinch={{ step: 5 }} doubleClick={{ disabled: true }}>
-            {({ state }) => (
-              <TransformComponent
-                wrapperStyle={{ width: "100%", height: "100%", overflow: "hidden" }}
-                contentStyle={{ width: "fit-content", height: "fit-content" }}
-              >
-                <div className="relative inline-block">
-                  <img src={`/maps/8.png`} alt="illustration" className="block" />
-                  <div
-                    className="absolute top-0 left-0 w-full h-full cursor-pointer"
-                  />
-                </div>
-              </TransformComponent>
-            )}
-          </TransformWrapper>
-        </div>
-
+        <ZoomableMap aspect={'1.38'} imgSrc={`/maps/8.png`} />
 
         <div className="flex justify-between items-center mb-4 mt-12">
           <p className="text-xl font-semibold">2体</p>
         </div>
+        <ZoomableMap aspect={'1.2'} imgSrc={`/maps/9.png`} />
 
-        <div className="w-full aspect-[1.2]">
-          <TransformWrapper wheel={{ step: 0.1 }} pinch={{ step: 5 }} doubleClick={{ disabled: true }}>
-            {({ state }) => (
-              <TransformComponent
-                wrapperStyle={{ width: "100%", height: "100%", overflow: "hidden" }}
-                contentStyle={{ width: "fit-content", height: "fit-content" }}
-              >
-                <div className="relative inline-block">
-                  <img src={`/maps/9.png`} alt="illustration" className="block" />
-                  <div
-                    className="absolute top-0 left-0 w-full h-full cursor-pointer"
-                  />
-                </div>
-              </TransformComponent>
-            )}
-          </TransformWrapper>
-        </div>
       </div>
     </div>
   )
