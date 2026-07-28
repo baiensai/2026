e9a285c530f2493ad308376353b2155db91ed832
Cosmic-rare <tani516158@gmail.com>
2026-07-28 15:11:58 +0900

    setup wrangler

--- next.config.mjs
+++ next.config.mjs
@@ -1,6 +1,13 @@
 import withFlowbiteReact from "flowbite-react/plugin/nextjs";
 
 /** @type {import('next').NextConfig} */
-const nextConfig = {};
+const nextConfig = {
+  output: "export",
+
+  // 今後next/imageを使ってもStatic Exportできるようにする
+  images: {
+    unoptimized: true,
+  },
+};
 
 export default withFlowbiteReact(nextConfig);
