a414f970b8508d0b3b2141e06286cf0fcf1e04f2
Cosmic-rare <tani516158@gmail.com>
2026-07-29 09:34:57 +0900

    Import latest site from successor

--- next.config.mjs
+++ next.config.mjs
@@ -1,10 +1,8 @@
 import withFlowbiteReact from "flowbite-react/plugin/nextjs";
 
-/** @type {import('next').NextConfig} */
+/** @type {import("next").NextConfig} */
 const nextConfig = {
   output: "export",
-
-  // 今後next/imageを使ってもStatic Exportできるようにする
   images: {
     unoptimized: true,
   },
