983519731527f9552ccca2951d7a0be8806cc60f
Cosmic-rare <tani516158@gmail.com>
2026-08-10 11:31:50 +0900

    setup cloudflare worker

--- next.config.mjs
+++ next.config.mjs
@@ -1,6 +1,12 @@
 import withFlowbiteReact from "flowbite-react/plugin/nextjs";
 
 /** @type {import('next').NextConfig} */
-const nextConfig = {};
+const nextConfig = {
+  output: "export",
+
+  images: {
+    unoptimized: true,
+  },
+};
 
 export default withFlowbiteReact(nextConfig);
